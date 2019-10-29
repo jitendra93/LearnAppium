e40486085e621a13d2ee0023ce0d8ad65b026c81
jitendraalekar <jitendraalekar@gmail.com>
2019-10-29 19:12:22 +0530

    Using constants

--- src/com/jitendraalekar/test/ConnectandFirstRun.kt
+++ src/com/jitendraalekar/test/ConnectandFirstRun.kt
@@ -3,6 +3,7 @@ package com.jitendraalekar.test
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.remote.MobileCapabilityType
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.remote.DesiredCapabilities
@@ -23,11 +24,11 @@ class ConnectandFirstRun {
 
         println(app.absolutePath)
         val capabilities = DesiredCapabilities()
-        capabilities.setCapability("automationName", "UiAutomator2")
-        capabilities.setCapability("platformName", "Android")
-        capabilities.setCapability("platformVersion", "10")
-        capabilities.setCapability("deviceName", "Android Emulator")
-        capabilities.setCapability("app", app.absolutePath)
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2")
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android")
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10")
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator")
+        capabilities.setCapability(MobileCapabilityType.APP, app.absolutePath)
         val wd = AppiumDriver<MobileElement>(URL("http://0.0.0.0:4723/wd/hub"), capabilities)
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS)
 
